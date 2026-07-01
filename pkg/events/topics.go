01515a6ad26f98301753aedeee39ea3d47179f04
Sokol111 <igorsokol111@gmail.com>
2026-07-01 15:49:10 +0200

    u

--- pkg/events/topics.go
+++ pkg/events/topics.go
@@ -2,11 +2,39 @@ package events
 
 import (
 	eventsv1 "github.com/Sokol111/ecommerce-tenant-service-api/gen/events/tenant/v1"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
-// TopicMap maps proto message full names to their Kafka topics.
-var TopicMap = map[protoreflect.FullName]string{
+// Topic constants
+const (
+	TopicTenantTenantEvents = "tenant.tenant.events"
+)
+
+// topicMap maps proto message full names to their Kafka topics.
+var topicMap = map[protoreflect.FullName]string{
 	(&eventsv1.TenantUpdatedEvent{}).ProtoReflect().Descriptor().FullName(): TopicTenantTenantEvents,
 	(&eventsv1.TenantDeletedEvent{}).ProtoReflect().Descriptor().FullName(): TopicTenantTenantEvents,
+}
+
+func init() {
+	fd := eventsv1.File_tenant_v1_events_proto
+	msgs := fd.Messages()
+	for i := range msgs.Len() {
+		fullName := msgs.Get(i).FullName()
+		if _, ok := topicMap[fullName]; !ok {
+			panic("events: no topic registered for " + string(fullName))
+		}
+	}
+}
+
+// TopicFor returns the Kafka topic for the given proto message.
+// Panics if the message type is not registered in topicMap.
+func TopicFor(msg proto.Message) string {
+	fullName := msg.ProtoReflect().Descriptor().FullName()
+	topic, ok := topicMap[fullName]
+	if !ok {
+		panic("events: no topic registered for " + string(fullName))
+	}
+	return topic
 }
